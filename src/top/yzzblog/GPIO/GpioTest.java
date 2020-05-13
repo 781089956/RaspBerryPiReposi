2592a978a26b5cd97f8a569cda61e1a8cf21d03e
781089956 <781089956@qq.com>
2020-05-13 15:06:04 +0800

    add !

--- src/top/yzzblog/GPIO/GpioTest.java
+++ src/top/yzzblog/GPIO/GpioTest.java
@@ -2,6 +2,6 @@ package top.yzzblog.GPIO;
 
 public class GpioTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
